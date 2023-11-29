285a1f2d36a452087ddd04c64e4d95835f2a92a6
mhshoara <mhshoara@yahoo.com>
2023-11-29 11:45:01 +0330

    update tss by fastecdsa

--- tss.py
+++ tss.py
@@ -1,13 +1,15 @@
-from web3 import Web3
-from ecpy.curves import Curve, Point
-from ecpy.keys import ECPublicKey, ECPrivateKey
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
-from typing import List, Dict
 
-import secrets
+from fastecdsa.encoding.sec1 import SEC1Encoder
+from fastecdsa import keys , curve
+from fastecdsa.curve import Curve
+from fastecdsa.point import Point
+from typing import List, Dict
+from web3 import Web3
+
 import math
 import json
 import base64
@@ -15,8 +17,8 @@ import base64
 
 class TSS:
 
-    curve: Curve = Curve.get_curve('secp256k1')
-    N: int = curve.order
+    ecurve: Curve = curve.secp256k1
+    N: int = ecurve.q
     Half_N: int = ((N >> 1) % N + 1) % N
 
     def __init__(self) -> None:
@@ -47,56 +49,55 @@ class TSS:
         return inverse
 
     @staticmethod
-    def pub_to_addr(public_key: ECPublicKey) -> str:
-        pub_key_hex = str(hex(public_key.W.x))[
-            2:] + str(hex(public_key.W.y))[2:]
+    def pub_to_addr(public_key: Point) -> str:
+        pub_key_hex = str(hex(public_key.x))[
+            2:] + str(hex(public_key.y))[2:]
         pub_hash = Web3.keccak(int(pub_key_hex, 16))
         return Web3.to_checksum_address('0x'+str(pub_hash.hex())[-40:])
 
     @staticmethod
-    def pub_to_code(public_key: ECPublicKey) -> int:
-        comp_pub = bytes(TSS.curve.encode_point(public_key.W, compressed=True))
+    def pub_to_code(public_key: Point) -> int:
+        comp_pub = SEC1Encoder.encode_public_key(public_key , True)
         return int(comp_pub.hex(), 16)
 
     @staticmethod
-    def code_to_pub(key: int) -> ECPublicKey:
-        key_byte = list(bytes.fromhex(hex(key).replace('x', '')))
-        return ECPublicKey(TSS.curve.decode_point(key_byte))
+    def code_to_pub(key: int) -> Point:
+        key_byte = bytes.fromhex(hex(key).replace('x', ''))
+        return SEC1Encoder.decode_public_key(key_byte , TSS.ecurve)
 
     @staticmethod
-    def private_to_point(private_key: ECPrivateKey) -> Point:
-        return TSS.curve.mul_point(private_key.d, TSS.curve.generator)
+    def private_to_point(private_key: int) -> Point:
+        return keys.get_public_key(private_key, TSS.ecurve)
 
     @staticmethod
-    def pub_compress(public_key: ECPublicKey) -> Dict:
-        coded = TSS.curve.encode_point(public_key.W, True)
-        x = bytearray(coded[1:]).hex()
-        y = coded[0] - 2
+    def pub_compress(public_key: Point) -> Dict:
+        coded = SEC1Encoder.encode_public_key(public_key , True)
+        x = '0x' + coded.hex()[2:]
+        y = int(coded.hex()[1]) - 2 
         return {'x': x, 'y_parity': y}
 
     @staticmethod
-    def pub_decompress(pub_dict: Dict) -> ECPublicKey:
+    def pub_decompress(pub_dict: Dict) -> Point:
         x = pub_dict['x']
         y = pub_dict['y_parity'] + 2
-        coded_x = [int(x[i:i+2], base=16) for i in range(0, len(x), 2)]
-        publicKey = TSS.curve.decode_point([y] + coded_x)
-        return ECPublicKey(publicKey)
+        coded = '0' + str(y) + x[2:]
+        publicKey = SEC1Encoder.decode_public_key(bytes.fromhex(coded) , TSS.ecurve)
+        return publicKey
 
     @staticmethod
-    def calc_poly_point(polynomial: List[ECPublicKey], x: int) -> Point:
+    def calc_poly_point(polynomial: List[Point], x: int) -> Point:
         x = int(x)
         coefs = []
         for i in range(len(polynomial)):
             coefs.append(pow(x, i) % TSS.N)
-        result = TSS.curve.mul_point(coefs[0], polynomial[0].W)
+        result = coefs[0] * polynomial[0]
         for i in range(1, len(polynomial)):
-            result = TSS.curve.add_point(
-                result, TSS.curve.mul_point(coefs[i], polynomial[i].W))
+            result = result + coefs[i] * polynomial[i]
         return result
 
     @staticmethod
-    def generate_random_private() -> ECPrivateKey:
-        return ECPrivateKey(secrets.randbits(32*8), TSS.curve)
+    def generate_random_private() -> int:
+        return keys.gen_private_key(TSS.ecurve)
 
     @staticmethod
     def langrange_coef(index: int, threshold: int, shares: List[Dict], x: int) -> int:
@@ -117,12 +118,12 @@ class TSS:
         sum = 0
         for j in range(threshold):
             coef = TSS.langrange_coef(j, threshold, shares, x)
-            key = shares[j]['key'].d
+            key = shares[j]['key']
             sum = (sum + (key * coef % TSS.N)) % TSS.N
         return sum % TSS.N
 
     @staticmethod
-    def schnorr_hash(public_key: ECPublicKey, message: int) -> str:
+    def schnorr_hash(public_key: Point, message: int) -> str:
         address = TSS.pub_to_addr(public_key)
         addressBuff = str(address)[2:]
         msgBuff = str(hex(message))[2:]
@@ -130,9 +131,9 @@ class TSS:
         return Web3.keccak(int(totalBuff, 16))
 
     @staticmethod
-    def schnorr_sign(shared_private_key: ECPrivateKey, nounce_private: ECPrivateKey, nounce_public: ECPublicKey, message: int) -> Dict[str, int]:
+    def schnorr_sign(shared_private_key: int, nounce_private: int, nounce_public: Point, message: int) -> Dict[str, int]:
         e = int.from_bytes(TSS.schnorr_hash(nounce_public, message), 'big')
-        s = (nounce_private.d - e * shared_private_key.d) % TSS.N
+        s = (nounce_private - e * shared_private_key) % TSS.N
         return {'s': s, 'e': e}
 
     @staticmethod
@@ -150,15 +151,14 @@ class TSS:
         return {'s': int(s, 16), 'e': int(e, 16)}
 
     @staticmethod
-    def schnorr_verify(public_key: ECPublicKey, message: str, signature: str) -> bool:
+    def schnorr_verify(public_key: Point, message: str, signature: str) -> bool:
         if type(signature) == str:
             signature = TSS.split_signature(signature)
         if type(message) != int:
             message = int(message)
         assert signature['s'] < TSS.N, 'Signature must be reduced modulo N'
-        r_v = TSS.curve.add_point(TSS.curve.mul_point(
-            signature['s'], TSS.curve.generator), TSS.curve.mul_point(signature['e'], public_key.W))
-        e_v = TSS.schnorr_hash(ECPublicKey(r_v), message)
+        r_v = (signature['s']* TSS.ecurve.G) + (signature['e'] * public_key)
+        e_v = TSS.schnorr_hash(r_v, message)
         return int.from_bytes(e_v, 'big') == signature['e']
 
     @staticmethod
@@ -175,8 +175,8 @@ class TSS:
         return {'s': s, 'e': e}
 
     @staticmethod
-    def frost_single_sign(id: str, share: ECPrivateKey, nonce_d: ECPrivateKey, nonce_e: ECPrivateKey, message: str,
-                          commitments_dict: Dict[str, Dict[str, int]], group_key: ECPublicKey) -> Dict[str, int]:
+    def frost_single_sign(id: str, share: int, nonce_d: int, nonce_e: int, message: str,
+                          commitments_dict: Dict[str,Dict[str ,int]], group_key: Point) -> Dict[str, int]:
 
         # commitment = {id: {id , D(i) , E(i)}}
         party = []
@@ -184,17 +184,20 @@ class TSS:
         my_row = 0
         aggregated_public_nonce = None
         is_first = True
+        
         commitments_list = list(commitments_dict.values())
         commitments_hash = Web3.keccak(text=json.dumps(commitments_list))
         message_hash = Web3.keccak(text=message)
 
         for commitment in commitments_list:
-            nonce_d_public = TSS.code_to_pub(commitment['public_nonce_d']).W
-            nonce_e_public = TSS.code_to_pub(commitment['public_nonce_e']).W
-            assert TSS.curve.is_on_curve(
-                nonce_d_public), f"Nonce D from Node {commitment['id']} Not on Curve"
-            assert TSS.curve.is_on_curve(
-                nonce_e_public), f"Nonce E from Node {commitment['id']} Not on Curve"
+            nonce_d_public = TSS.code_to_pub(commitment['public_nonce_d'])
+            nonce_e_public = TSS.code_to_pub(commitment['public_nonce_e'])
+            assert TSS.ecurve.is_point_on_curve(
+                (nonce_d_public.x, nonce_d_public.y)
+                ), f"Nonce D from Node {commitment['id']} Not on Curve"
+            assert TSS.ecurve.is_point_on_curve(
+                (nonce_e_public.x, nonce_e_public.y)
+                ), f"Nonce E from Node {commitment['id']} Not on Curve"
 
             party.append(commitment['id'])
 
@@ -203,14 +206,12 @@ class TSS:
                 [hex(commitment['id']),  message_hash,  commitments_hash]
             )
 
-            public_nonce = TSS.curve.add_point(nonce_d_public, TSS.curve.mul_point(
-                int.from_bytes(row, 'big'), nonce_e_public))
+            public_nonce = nonce_d_public + (int.from_bytes(row, 'big') * nonce_e_public)
             if is_first:
                 aggregated_public_nonce = public_nonce
                 is_first = False
             else:
-                aggregated_public_nonce = TSS.curve.add_point(
-                    aggregated_public_nonce, public_nonce)
+                aggregated_public_nonce = aggregated_public_nonce + public_nonce
 
             if (id == commitment['id']):
                 my_row = row
@@ -224,23 +225,26 @@ class TSS:
                 'address'
             ],
             [
-                Web3.to_int(
-                    hexstr="0x" + TSS.pub_compress(TSS.code_to_pub(group_key))['x']),
+                Web3.to_int(hexstr= TSS.pub_compress(TSS.code_to_pub(group_key))['x']),
                 TSS.pub_compress(TSS.code_to_pub(group_key))['y_parity'],
                 Web3.to_int(message_hash),
-                TSS.pub_to_addr(ECPublicKey(aggregated_public_nonce))
+                TSS.pub_to_addr(aggregated_public_nonce)
             ]
         )
 
         coef = TSS.langrange_coef(index, len(party), commitments_list, 0)
-        signature_share = (nonce_d.d + nonce_e.d * int.from_bytes(my_row, 'big') -
-                           coef * share.d * int.from_bytes(challenge, 'big')) % TSS.N
+        signature_share = (nonce_d + nonce_e * int.from_bytes(my_row, 'big') -
+                           coef * share * int.from_bytes(challenge, 'big')) % TSS.N
 
-        return {'id': id, 'signature': signature_share, 'public_key': TSS.pub_to_code(share.get_public_key())}
+        return {
+            'id': id, 
+            'signature': signature_share, 
+            'public_key': TSS.pub_to_code(keys.get_public_key(share , TSS.ecurve))
+            }
 
     @staticmethod
     def frost_verify_single_signature(id: int, message: str, commitments_dict: Dict[str, Dict[str, int]], aggregated_public_nonce: Point,
-                                      public_key_share: int, single_signature: Dict[str, int], group_key: ECPublicKey) -> bool:
+                                      public_key_share: int, single_signature: Dict[str, int], group_key: Point) -> bool:
 
         index = 0
         public_nonce = None
@@ -250,14 +254,13 @@ class TSS:
 
         for commitment in commitments_list:
             if commitment['id'] == id:
-                nonce_d_public = TSS.code_to_pub(commitment['public_nonce_d']).W
-                nonce_e_public = TSS.code_to_pub(commitment['public_nonce_e']).W
+                nonce_d_public = TSS.code_to_pub(commitment['public_nonce_d'])
+                nonce_e_public = TSS.code_to_pub(commitment['public_nonce_e'])
                 row = Web3.solidity_keccak(
                     ['string', 'bytes', 'bytes'],
                     [hex(commitment['id']),  message_hash,  commitments_hash]
                 )
-                public_nonce = TSS.curve.add_point(nonce_d_public, TSS.curve.mul_point(
-                    int.from_bytes(row, 'big'), nonce_e_public))
+                public_nonce = nonce_d_public + (int.from_bytes(row, 'big') * nonce_e_public)
                 index = commitments_list.index(commitment)
 
         challenge = Web3.solidity_keccak(
@@ -268,27 +271,23 @@ class TSS:
                 'address'
             ],
             [
-                Web3.to_int(
-                    hexstr="0x" + TSS.pub_compress(TSS.code_to_pub(group_key))['x']),
+                Web3.to_int(hexstr=TSS.pub_compress(TSS.code_to_pub(group_key))['x']),
                 TSS.pub_compress(TSS.code_to_pub(group_key))['y_parity'],
                 Web3.to_int(message_hash),
-                TSS.pub_to_addr(ECPublicKey(aggregated_public_nonce))
+                TSS.pub_to_addr(aggregated_public_nonce)
             ]
         )
 
-        coef = TSS.langrange_coef(index, len(
-            commitments_list), commitments_list, 0)
+        coef = TSS.langrange_coef(index, len(commitments_list), commitments_list, 0)
 
-        point1 = TSS.curve.sub_point(public_nonce, TSS.curve.mul_point(
-            int.from_bytes(challenge, 'big')*coef, TSS.code_to_pub(public_key_share).W))
-        point2 = TSS.curve.mul_point(
-            single_signature['signature'], TSS.curve.generator)
+        point1 = public_nonce - (int.from_bytes(challenge, 'big')* coef * TSS.code_to_pub(public_key_share))
+        point2 = single_signature['signature'] * TSS.ecurve.G
 
         return point1 == point2
 
     @staticmethod
     def frost_aggregate_signatures(single_signatures: List[Dict[str, int]], public_key_shares: Dict[str, int], message: str, 
-                                   commitments_dict: Dict[str, Dict[str, int]], group_key: ECPublicKey) -> Dict:
+                                   commitments_dict: Dict[str, Dict[str, int]], group_key: Point) -> Dict:
 
         aggregated_signature = 0
         aggregated_public_nonce = None
@@ -298,22 +297,20 @@ class TSS:
         message_hash = Web3.keccak(text=message)
 
         for commitment in commitments_list:
-            nonce_d_public = TSS.code_to_pub(commitment['public_nonce_d']).W
-            nonce_e_public = TSS.code_to_pub(commitment['public_nonce_e']).W
+            nonce_d_public = TSS.code_to_pub(commitment['public_nonce_d'])
+            nonce_e_public = TSS.code_to_pub(commitment['public_nonce_e'])
 
             row = Web3.solidity_keccak(
                 ['string', 'bytes', 'bytes'],
                 [hex(commitment['id']),  message_hash,  commitments_hash]
             )
 
-            public_nonce = TSS.curve.add_point(nonce_d_public, TSS.curve.mul_point(
-                int.from_bytes(row, 'big'), nonce_e_public))
+            public_nonce = nonce_d_public + (int.from_bytes(row, 'big') * nonce_e_public)
             if is_first:
                 aggregated_public_nonce = public_nonce
                 is_first = False
             else:
-                aggregated_public_nonce = TSS.curve.add_point(
-                    aggregated_public_nonce, public_nonce)
+                aggregated_public_nonce = aggregated_public_nonce + public_nonce
 
         for sign in single_signatures:
             if TSS.frost_verify_single_signature(sign['id'], message, commitments_dict, aggregated_public_nonce, public_key_shares[sign['id']], sign, group_key):
@@ -322,7 +319,7 @@ class TSS:
                 print(
                     f"Failed to sign. Signature from {sign['id']} is not verified.")
         aggregated_signature = aggregated_signature % TSS.N
-        return {'nonce': TSS.pub_to_addr(ECPublicKey(aggregated_public_nonce)), 'public_key': TSS.pub_compress(TSS.code_to_pub(group_key)),
+        return {'nonce': TSS.pub_to_addr(aggregated_public_nonce), 'public_key': TSS.pub_compress(TSS.code_to_pub(group_key)),
                 'signature': aggregated_signature, 'message_hash': message_hash}
 
     @staticmethod
@@ -336,44 +333,37 @@ class TSS:
                 'address'
             ],
             [
-                Web3.to_int(
-                    hexstr='0x' + aggregated_signature['public_key']['x']),
+                Web3.to_int(hexstr= aggregated_signature['public_key']['x']),
                 aggregated_signature['public_key']['y_parity'],
                 Web3.to_int(aggregated_signature['message_hash']),
                 aggregated_signature['nonce']
             ]
         )
 
-        point = TSS.curve.add_point(
-            TSS.curve.mul_point(
-                aggregated_signature['signature'],
-                TSS.curve.generator),
-            TSS.curve.mul_point(
-                int.from_bytes(challenge, 'big'),
-                TSS.pub_decompress(aggregated_signature['public_key']).W))
-        return aggregated_signature['nonce'] == TSS.pub_to_addr(ECPublicKey(point))
-
+        point = (aggregated_signature['signature'] * TSS.ecurve.G) + (
+            int.from_bytes(challenge, 'big') * TSS.pub_decompress(aggregated_signature['public_key']))
+        return aggregated_signature['nonce'] == TSS.pub_to_addr(point)
     @staticmethod
-    def complaint_sign(private_key : ECPrivateKey , nonce : ECPrivateKey , hash : int):
-        return (nonce.d + private_key.d * hash) % TSS.N 
+    def complaint_sign(private_key : int , nonce : int , hash : int):
+        return (nonce + private_key * hash) % TSS.N 
     
     @staticmethod
-    def complaint_verify(public_complaintant : ECPublicKey, public_malicious : ECPublicKey , encryption_key : ECPublicKey , proof , hash : int):
+    def complaint_verify(public_complaintant : Point, public_malicious : Point , encryption_key : Point , proof , hash : int):
         public_nonce = proof['public_nonce']
         public_commitment = proof['commitment']
         signature = proof['signature']
         
-        point1 = TSS.curve.add_point(public_nonce.W, TSS.curve.mul_point(hash, public_complaintant.W))
-        point2 = TSS.curve.mul_point(signature, TSS.curve.generator)
+        point1 = public_nonce + (hash * public_complaintant)
+        point2 = signature * TSS.ecurve.G
         verification1 = (point1 == point2)
         
-        point1 = TSS.curve.add_point(public_commitment.W, TSS.curve.mul_point(hash, encryption_key.W))
-        point2 = TSS.curve.mul_point(signature, public_malicious.W)
+        point1 = public_commitment + (hash * encryption_key)
+        point2 = signature * public_malicious
         verification2 = (point1 == point2)
         
         return verification1 and verification2
     
-    
+
     @staticmethod
     def generate_hkdf_key(key: int) -> bytes:
         hkdf = HKDF(algorithm=hashes.SHA256(), length=32,
