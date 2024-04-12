db5961d382d189787046ff69ee1d13a3bebc4533
Abram Symons <abramsymons@gmail.com>
2024-04-12 16:24:51 +0330

    add missed setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['pyfrost/tests', 'pyfrost/example']),
     install_requires=[
-        'web3==5.31.4',
-        'ecpy==1.2.5',
-        'numpy==1.26.2',
-        'libp2p @ git+https://github.com/libp2p/py-libp2p.git@b38b36862f44421aec998e438b668cff265de75c#egg=libp2p',
-        'Flask==3.0.0'
+        'fastecdsa==2.2.3',
+        'Flask==3.0.0',
+        'cryptography',
     ],
 )
